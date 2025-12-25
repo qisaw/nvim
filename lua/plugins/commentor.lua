50f933d9fc121eb4cc8d02a4d52b41e9a8e8d7c8
Wasiq Kashkari <w.kashkari@outlook.com>
2025-12-26 05:23:01 +1300

    upgrade lazy-lock and add some comments

--- lua/plugins/commentor.lua
+++ lua/plugins/commentor.lua
@@ -3,6 +3,12 @@ return {
 		"numToStr/Comment.nvim",
 		opts = {
 			-- add any options here
+			-- to comment
+			--   gcc in normal model
+			--   gc in visual model
+			-- To Comment block
+			--   gcb in normal model
+			--   gb in visual model
 		},
 	},
 }
