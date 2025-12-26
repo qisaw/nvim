cbac50b3d63e681840e38ddfb5a130c9d0ce38ad
Wasiq Kashkari <w.kashkari@outlook.com>
2025-12-26 13:30:20 +1300

    Update light and dark mode themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,18 +3,47 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
+		lazy = false,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "macchiato", -- latte, frappe, macchiato, mocha
+				flavour = "frappe",
+				background = {
+					light = "latte",
+					dark = "frappe",
+				},
 				integrations = {
 					cmp = true,
 					gitsigns = true,
 					neotree = true,
 					treesitter = true,
-					mason = true,
+					notify = true,
 				},
 			})
-			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"neanias/everforest-nvim",
+		name = "everforest",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("everforest").setup({
+				background = "soft",
+			})
+
+			local function set_theme()
+				if vim.o.background == "light" then
+					vim.cmd.colorscheme("everforest")
+				else
+					vim.cmd.colorscheme("catppuccin-frappe")
+				end
+			end
+
+			set_theme()
+			vim.api.nvim_create_autocmd("OptionSet", {
+				pattern = "background",
+				callback = set_theme,
+			})
 		end,
 	},
 }
