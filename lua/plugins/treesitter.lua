9a513f78ee46c845c8b5b0357ae690955785ed7d
Wasiq Kashkari <w.kashkari@outlook.com>
2026-04-17 10:42:31 +1200

    formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,57 +1,55 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 local treesitter = require("nvim-treesitter")
 
 treesitter.install({
-  -- Neovim
-  "lua",
-  "vim",
-  "vimdoc",
+	-- Neovim
+	"lua",
+	"vim",
+	"vimdoc",
 
-  -- Web / Next.js
-  "typescript",
-  "tsx",
-  "javascript",
-  "json",
-  "yaml",
-  "html",
-  "css",
+	-- Web / Next.js
+	"typescript",
+	"tsx",
+	"javascript",
+	"json",
+	"yaml",
+	"html",
+	"css",
 
-  -- DevOps / infra
-  "dockerfile",
-  "terraform",
-  "hcl",
+	-- DevOps / infra
+	"dockerfile",
+	"terraform",
+	"hcl",
 
-  -- DB / tools
-  "prisma",
+	-- DB / tools
+	"prisma",
 
-  -- For AI tools
-  "yaml",
-  "markdown",
-  "markdown_inline",
+	-- For AI tools
+	"markdown",
+	"markdown_inline",
 })
-
 
 local grp = vim.api.nvim_create_augroup("TSMain", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-  group = grp,
-  callback = function(ev)
-    local lang = vim.treesitter.language.get_lang(ev.match)
-    if lang and vim.treesitter.query.get(lang, "highlights") then
-      vim.treesitter.start(ev.buf, lang)
-    end
-    -- indent via Treesitter indentexpr (if indent queries exist)
-    -- if lang and vim.treesitter.query.get(lang, "indents") then
-    -- 	vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    -- end
-    if lang and vim.treesitter.query.get(lang, "folds") then
-      local win = vim.api.nvim_get_current_win()
-      vim.wo[win].foldmethod = "expr"
-      vim.wo[win].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-      vim.wo[win].foldenable = true -- disable folding by default
-      vim.wo[win].foldlevel = 99 -- open all folds by default
-    end
-  end,
+	group = grp,
+	callback = function(ev)
+		local lang = vim.treesitter.language.get_lang(ev.match)
+		if lang and vim.treesitter.query.get(lang, "highlights") then
+			vim.treesitter.start(ev.buf, lang)
+		end
+		-- indent via Treesitter indentexpr (if indent queries exist)
+		-- if lang and vim.treesitter.query.get(lang, "indents") then
+		-- 	vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		-- end
+		if lang and vim.treesitter.query.get(lang, "folds") then
+			local win = vim.api.nvim_get_current_win()
+			vim.wo[win].foldmethod = "expr"
+			vim.wo[win].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo[win].foldenable = true -- disable folding by default
+			vim.wo[win].foldlevel = 99 -- open all folds by default
+		end
+	end,
 })
